a2335b851a8538b2ae1207d87a190dd650b49bc8
Jeff Whitaker <jswhit@fastmail.fm>
2024-01-18 16:02:05 -0700

    update

--- test.py
+++ test.py
@@ -1,17 +1,4 @@
 import xarray as xr
-from redtoreg import redtoreg
-import numpy as np
-
-# tracer variables on gaussian grid.
 ds_era5t=xr.open_zarr("gs://gcp-public-data-arco-era5/co/model-level-moisture.zarr-v2",consolidated=True,)
-q = ds_era5t['q'][-1,0,:].values
-lats = ds_era5t['latitude'][:].values
-lats_distinct = np.unique(lats)
-nlats = len(lats_distinct)
-lonsperlat = np.empty(nlats, int)
-for j in range(nlats):
-    lonsperlat[j] = np.isclose(lats, lats_distinct[j]).sum()
-print(lonsperlat)
-qq = redtoreg(q,lonsperlat,missval=9999)
-print(q.shape, q.dtype, q.min(), q.max())
-print(qq.shape, qq.dtype, qq.min(), qq.max())
+da = ds_era5t['q'][-1,...]
+da.to_netcdf('test.nc')
