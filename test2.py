707834ea04d36b53c2dd41a81afc577296025f70
jswhit <whitaker.jeffrey@gmail.com>
2024-01-19 09:56:26 -0700

    update

--- test2.py
+++ test2.py
@@ -7,6 +7,7 @@ import time
 nc = Dataset('test.nc')
 q = nc['q'][:]
 print(q.shape)
+
 lats = nc['latitude']
 lats_distinct = np.unique(lats)
 nlats = len(lats_distinct)
@@ -14,13 +15,16 @@ lonsperlat = np.empty(nlats, int)
 for j in range(nlats):
     lonsperlat[j] = np.isclose(lats, lats_distinct[j]).sum()
 print(lonsperlat)
+
 nlons = lonsperlat.max()
 nlevs = q.shape[0]
 qq = np.empty((nlevs,nlats,nlons), q.dtype)
+
 start = time.time()
 for k in range(nlevs):
     qq[k] = redtoreg(q[k],lonsperlat)
 end = time.time()
 elapsed = end-start
+
 print('time in redtoreg2 =',elapsed)
 print(qq.shape, qq.min(), qq.max())
